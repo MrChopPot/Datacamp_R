e036425aab449093d316ad1f8cc8fbc330d478d7
Conrad Kang <43460783+MrChopPot@users.noreply.github.com>
2019-03-14 12:00:38 -0400

    Create SVM.R

---
+++ SVM.R
@@ -0,0 +1,77 @@
+### Support Vector Machine in R
+
+# 1. Introduction
+
+# Load ggplot2
+library(ggplot2)
+
+# Print variable names
+print(colnames(df))
+
+# Plot sugar content along the x-axis
+plot_df <- ggplot(data = df, aes(x = sugar_content, y = 0)) + 
+    geom_point() + 
+    geom_text(aes(label = sugar_content), size = 2.5, vjust = 2, hjust = 0.5)
+
+# Display plot
+plot_df
+
+#The maximal margin separator is at the midpoint of the two extreme points in each cluster.
+mm_separator <- (8.9 + 10)/2
+
+#create data frame containing the maximum margin separator
+separator <- data.frame(sep = mm_separator)
+
+#add ggplot layer 
+plot_sep <- plot_ + geom_point(data = separator, aes(x = sep, y = 0), color = "blue", size = 4)
+
+#display plot
+plot_sep
+
+#set seed
+set.seed(42)
+
+#set number of data points. 
+n <- 600
+
+#Generate data frame with two uniformly distributed predictors lying between 0 and 1.
+df <- data.frame(x1 = runif(600), 
+                 x2 = runif(600))
+
+#classify data points depending on location
+df$y <- factor(ifelse(df$x2 - 1.4*df$x1 < 0, -1, 1), 
+    levels = c(-1, 1))
+
+#set margin
+delta <- .07
+
+# retain only those points that lie outside the margin
+df1 <- df[abs(1.4*df$x1 - df$x2) > delta, ]
+
+#build plot
+plot_margins <- ggplot(data = df1, aes(x = x1, y = x2, color = y)) + geom_point() + 
+    scale_color_manual(values = c("red", "blue")) + 
+    geom_abline(slope = 1.4, intercept = 0)+
+    geom_abline(slope = 1.4, intercept = delta, linetype = "dashed") +
+    geom_abline(slope = 1.4, intercept = -delta, linetype = "dashed")
+ 
+#display plot 
+plot_margins
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
