ee3d29f3eef80df8b3bfc46d4a3e5a81ca86c440
Conrad Kang <43460783+MrChopPot@users.noreply.github.com>
2019-03-18 19:45:53 -0400

    Update SL_Case_Study.R

--- SL_Case_Study.R
+++ SL_Case_Study.R
@@ -213,7 +213,7 @@ voters %>%
               `Economy is getting better` = mean(econtrend_2016 == 1),
               `Crime is very important` = mean(imiss_a_2016 == 2))
 
-## Visualize difference by voter turnout
+# Visualize difference by voter turnout
 voters %>%
     ggplot(aes(econtrend_2016, ..density.., fill = turnout16_2016)) +
     geom_histogram(alpha = 0.5, position = "identity", binwidth = 1) +
