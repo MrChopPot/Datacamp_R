5c398771cc0717a3012776c20d0ed758a5d5b994
Conrad Kang <43460783+MrChopPot@users.noreply.github.com>
2019-03-18 00:04:18 -0400

    Update SL_Case_Study.R

--- SL_Case_Study.R
+++ SL_Case_Study.R
@@ -93,4 +93,104 @@ results %>%
 
 #######################
 
-### 2. 
+### 2. Stack Overflow Developer Survey
+
+# Print stackoverflow
+stackoverflow
+
+# First count for Remote
+stackoverflow %>% 
+    count(Remote, sort = TRUE)
+
+# then count for Country
+stackoverflow %>% 
+    count(Country, sort = TRUE)
+
+ggplot(stackoverflow, aes(x = Remote, y = YearsCodedJob)) +
+    geom_boxplot() +
+    labs(x = NULL,
+         y = "Years of professional coding experience") 
+
+# Build a simple logistic regression model
+simple_glm <- stackoverflow %>%
+        select(-Respondent) %>%
+        glm(Remote ~ .,
+            family = "binomial",
+            data = .)
+
+# Print the summary of the model
+summary(simple_glm)
+
+# Load caret
+library(caret)
+
+# Create stack_select dataset
+stack_select <- stackoverflow %>%
+    select(-Respondent)
+
+# Split the data into training and testing sets
+set.seed(1234)
+in_train <- createDataPartition(stack_select$Remote, p = .8, list = FALSE)
+training <- stack_select[in_train,]
+testing <- stack_select[-in_train,]
+
+# Create the upsampled training set
+up_train <- upSample(x = select(training, -Remote),
+                     y = training$Remote,
+                     yname = "Remote") %>%
+    as_tibble()
+
+# Count the number of each type of Remote employee
+up_train %>%
+    count(Remote)
+
+# Build a logistic regression model
+stack_glm <- train(Remote ~ ., method = "glm", family = "binomial",
+                   data = training,
+                   trControl = trainControl(method = "boot",
+                                            sampling = "up"))
+
+# Print the model object 
+stack_glm
+
+# Build a random forest model
+stack_rf <- train(Remote ~ ., method = "rf", 
+                  data = training,
+                  trControl = trainControl(method = "boot",
+                                           sampling = "up"))
+
+# Print the model object
+stack_rf
+
+# Set seed
+set.seed(123)
+
+# Confusion matrix for logistic regression model
+confusionMatrix(predict(stack_glm, testing),
+                testing$Remote)
+
+# Set seed
+set.seed(123)
+
+# Confusion matrix for random forest model
+confusionMatrix(predict(stack_rf, testing),
+                testing$Remote)
+
+# Load yardstick
+library(yardstick)
+
+# Predict values
+testing_results <- testing %>%
+    mutate(`Logistic regression` = predict(stack_glm, testing),
+           `Random forest` = predict(stack_rf, testing))
+
+## Calculate accuracy
+accuracy(testing_results, truth = Remote, estimate = `Logistic regression`)
+accuracy(testing_results, truth = Remote, estimate = `Random forest`)
+
+## Calculate positive predict value
+ppv(testing_results, truth = Remote, estimate = `Logistic regression`)
+ppv(testing_results, truth = Remote, estimate = `Random forest`)
+
+
+
