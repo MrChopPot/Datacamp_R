8aff912ee22047757ab8ac574366afc07bbdf83a
Conrad Kang <43460783+MrChopPot@users.noreply.github.com>
2019-04-20 02:12:15 -0400

    Update Efficient_R.R

--- Efficient_R.R
+++ Efficient_R.R
@@ -82,31 +82,142 @@ microbenchmark(mat[1, ], df[1, ])
 
 ### 3. Diagnosing Problems: Code Profiling
 
+# Load the data set
+data(movies, package = "ggplot2movies") 
 
+# Load the profvis package
+library(profvis)
 
+# Profile the following code with the profvis function
+profvis({
+  # Load and select data
+  movies <- movies[movies$Comedy == 1, ]
 
+  # Plot data of interest
+  plot(movies$year, movies$rating)
 
+  # Loess regression line
+  model <- loess(rating ~ year, data = movies)
+  j <- order(movies$year)
+  
+  # Add a fitted line to the plot
+  lines(movies$year[j], model$fitted[j], col = "red")
+})     ## Remember the closing brackets!
 
+# Load the microbenchmark package
+library(microbenchmark)
 
+# The previous data frame solution is defined
+# d() Simulates 6 dices rolls
+d <- function() {
+  data.frame(
+    d1 = sample(1:6, 3, replace = TRUE),
+    d2 = sample(1:6, 3, replace = TRUE)
+  )
+}
 
+# Complete the matrix solution
+m <- function() {
+  matrix(sample(1:6, 6, replace = TRUE), ncol = 2)
+}
 
+# Use microbenchmark to time m() and d()
+microbenchmark(
+ data.frame_solution = d(),
+ matrix_solution     = m()
+)
 
+# Example data
+rolls
 
+# Define the previous solution 
+app <- function(x) {
+    apply(x, 1, sum)
+}
 
+# Define the new solution
+r_sum <- function(x) {
+    rowSums(x)
+}
 
+# Compare the methods
+microbenchmark(
+    app_sol = app(rolls),
+    r_sum_sol = r_sum(rolls)
+)
 
+# Example data
+is_double
 
+# Define the previous solution
+move <- function(is_double) {
+    if (is_double[1] & is_double[2] & is_double[3]) {
+        current <- 11 # Go To Jail
+    }
+}
 
+# Define the improved solution
+improved_move <- function(is_double) {
+    if (is_double[1] && is_double[2] && is_double[3]) {
+        current <- 11 # Go To Jail
+    }
+}
 
+# microbenchmark both solutions
+# Very occassionally the improved solution is actually a little slower
+# This is just random chance
+microbenchmark(move, improved_move, times = 1e5)
 
+#######################
 
+### 4. Turbo Charged Code: Parallel Programming
 
+# Load the parallel package
+library(parallel)
 
+# Store the number of cores in the object no_of_cores
+no_of_cores <- detectCores()
 
+# Print no_of_cores
+no_of_cores
 
+# Determine the number of available cores.
+detectCores()
 
+# Create a cluster via makeCluster
+cl <- makeCluster(2)
 
+# Parallelize this code
+parApply(cl, dd, 2, median)
 
+# Stop the cluster
+stopCluster(cl)
 
+library("parallel")
+# Create a cluster via makeCluster (2 cores)
+cl <- makeCluster(2)
 
+# Export the play() function to the cluster
+clusterExport(cl, "play")
 
+# Re-write sapply as parSapply
+res <- parSapply(cl, 1:100, function(i) play())
+
+# Stop the cluster
+stopCluster(cl)
+
+# Set the number of games to play
+no_of_games <- 1e5
+
+## Time serial version
+system.time(serial <- sapply(1:no_of_games, function(i) play()))
+
+## Set up cluster
+cl <- makeCluster(4)
+clusterExport(cl, "play")
+
+## Time parallel version
+system.time(par <- parSapply(cl, 1:no_of_games, function(i) play()))
+
+## Stop cluster
+stopCluster(cl)
